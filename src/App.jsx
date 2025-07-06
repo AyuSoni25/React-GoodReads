5b55d576c5d26215a4fcec0f0551e3842a3c1a4b
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2025-07-06 23:41:09 +0530

    Added books to shelf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import MainRoutes from 'Routes/MainRoutes';
 import './App.css';
+
+import MainRoutes from 'Routes/MainRoutes';
 
 function App() {
   return <MainRoutes/>;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from 'Redux/Slices/AuthSlice';
 import booksSliceReducer from 'Redux/Slices/BooksSlice';
+import shelfSliceReducer from 'Redux/Slices/ShelfSlice';
 
 export default configureStore({
     reducer: {
         auth: authSliceReducer,
-        books: booksSliceReducer
+        books: booksSliceReducer,
+        shelf: shelfSliceReducer
     },
     devTools: true,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
